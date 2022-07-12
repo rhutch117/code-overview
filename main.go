434217736cd614aaaf0035bfe125568f43ce37e0
Ryan Hutchison <rwhutchison@live.com>
2022-07-12 12:04:15 -0500

    feat: Iterate over lines in a file, printing any words in the line that exist in the list of keywords

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+var keywords = map[string]bool{
+	"struct": true,
+	"func":   true,
+}
 
 func main() {
-	fmt.Println("Hello")
+
+	file, err := os.Open("./exampleFile.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// limited to lines under 64k
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		ParseLine(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ParseLine(s string) {
+	words := strings.Fields(s)
+	for _, word := range words {
+		if IsKeyword(word, keywords) {
+			fmt.Println(word)
+		}
+	}
+}
+
+// Return whether the given word is a keyword or not
+func IsKeyword(s string, k map[string]bool) bool {
+	return k[s]
 }
